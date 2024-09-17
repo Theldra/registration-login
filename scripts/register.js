6d6a4c9f11996a5f5261b566a79d979ec6c53504
Etheldra Akahoho <etheldeak4@gmail.com>
2024-09-17 15:56:20 +0000

    New add ups

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,22 @@
 //Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register');
 //Handle form submit event
-registerForm.onsubmit = function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event);
+    //Collect form data
+    const formData = new FormData(registerForm);
+    //Send data to backend
+    //Display success message to user
+    const messageH1 = document.querySelector('#message');
+    messageH1.textContent = 'Account registered successfully';
+});
+
+//declare variable age
+// const age = 71;
+//define squareAge function
+function squareAge(age) {
+    return age ** 2;
 }
+const squaredAge = squareAge(54);
+console.log(squaredAge);
+
